03610d124d84438149e8dec2d81a7cbbedd3886c
hi <haxordp@gmail.com>
2026-06-09 21:49:28 +0200

    update schema

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,21 +14,7 @@ interface BeatmapScoreParams {
 
 // ------------------------------------------
 
-////// Was supposed to be used for pog!stats, but saving BeatmapScoreFull instead in case someone finds it useful
-// interface BeatmapScore {
-// 	id: number;
-// 	beatmapId: number;
-// 	position: number;
-// 	accuracy: number;
-// 	timestamp: number;
-// 	score: number;
-// 	scoreClassic: number;
-// 	pp?: number;
-// 	rank: ScoreRank;
-// 	mods: ApiMod[];
-// 	userId: number;
-// }
-////// Final table schema for pog!stats could be something like this:
+////// Final table schema for pog!stats could be smaller, e.g.:
 // id            bigint PRIMARY KEY,
 // beatmap_id    bigint NOT NULL,
 // position      smallint NOT NULL,
@@ -41,39 +27,24 @@ interface BeatmapScoreParams {
 // mods          jsonb NOT NULL,
 // user_id       integer NOT NULL
 
-////// pekkie schema
-//  	 id                  BIGINT NOT NULL,
-//     user_id             BIGINT NOT NULL,
-//     ruleset_id          SMALLINT NOT NULL,
-//     beatmap_id          BIGINT NOT NULL,
-//     has_replay          BOOLEAN NOT NULL DEFAULT FALSE,
-//     ranked              BOOLEAN NOT NULL DEFAULT TRUE,  <--- dropping this one
-//     rank                CHAR(2) NOT NULL DEFAULT '',
-//     accuracy            NUMERIC(6,3) NOT NULL DEFAULT 0, <--- changing to REAL
-//     max_combo           INTEGER NOT NULL DEFAULT 0,
-//     total_score         BIGINT NOT NULL DEFAULT 0,
-//     classic_total_score BIGINT,
-//     total_score_without_mods BIGINT,
-//     is_perfect_combo    BOOLEAN,
-//     legacy_perfect      BOOLEAN,
-//     pp                  DOUBLE PRECISION DEFAULT NULL, <--- changing to REAL
-//     legacy_total_score  BIGINT NOT NULL DEFAULT 0,
-//     ended_at            TIMESTAMPTZ NOT NULL,
-//     build_id            SMALLINT DEFAULT NULL,      <----- dropping this one
-//     stable              BOOLEAN NOT NULL DEFAULT FALSE,
-//     data                JSONB NOT NULL DEFAULT '{}'::jsonb,
+////// differences betweeen pekkie schema
+//     total_score - INTEGER instead of BIGINT
+//     total_score_without_mods - INTEGER instead of BIGINT
+//     pp - REAL instead of DOUBLE PRECISION
+//     build_id - dropped
+//     + 3 other meta columns: is_scraped, retrieved_at, position
 
 interface BeatmapScoreFull {
 	position: number; // meta, not from API
 	isScraped: boolean; // meta, not from API
 	retrievedAt: Date; // meta, not from API
-	stable: boolean; // meta, not from API (true only if build_id is present)
+	lazer: boolean; // meta, not from API (true only if build_id is present)
 	id: number;
 	userId: number;
 	rulesetId: number;
 	beatmapId: number;
 	hasReplay: boolean;
-	rank: ScoreRank;
+	grade: ScoreRank;
 	accuracy: number; // 0-1
 	maxCombo: number;
 	totalScore: number;
